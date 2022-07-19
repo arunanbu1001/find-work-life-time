80013556139afbf949fff2c1c6a0f986bf2584f4
arunanbu1001 <104549493+arunanbu1001@users.noreply.github.com>
2022-07-19 10:15:08 +0530

    Update date.js

--- date.js
+++ date.js
@@ -2,15 +2,76 @@ var month = document.getElementById("month")
 console.log(month);
 var days = document.getElementById('days')
 var year = document.getElementById("year")
-year.addEventListener('click',changeyr)
-function changeyr(){
-    yr=year.value
+var reage = document.getElementById("reage")
+var btn = document.getElementById("go")
+var di = document.getElementById('di')
+var body
+
+console.log(reage.value)
+//........................................
+year.addEventListener('click', changeyr)
+function changeyr() {
+    yr = year.value
     console.log(yr)
-    document.getElementById('month').value=''
-    document.getElementById('days').value=''
-     
-   
+    // console.log(typeof yr)
+    document.getElementById('month').value = ''
+    document.getElementById('days').value = ''
 }
+
+
+
+//button gooo..........................................
+
+btn.addEventListener('click', go)
+function go() {
+    di.innerHTML = '';
+    //age yr calculating............................
+    var reyr = new Date()
+    // console.log(reyr)
+    var curyr = (reyr.getUTCFullYear());
+    console.log(curyr)
+    // console.log(typeof curyr)
+    var curage = (curyr - yr)
+    console.log(curage)
+    var furage = (reage.value - curage)
+    console.log(furage)
+    //........................................... 
+
+
+
+    //age month calculating.................
+    var curmon = (reyr.getUTCMonth());
+    var balanceagemonth = Math.abs((12) - (month.value))
+    console.log(curmon)
+    console.log(balanceagemonth)
+    // console.log(curmon+1)
+    //......................................
+
+
+    //age days.......................................
+    var curday = (reyr.getUTCDate());
+    if (month.value == 1 || month.value == 3 || month.value == 5 || month.value == 7 || month.value == 8 || month.value == 10 || month.value == 12) {
+        var balanceageday = Math.abs((31) - (days.value))
+        // console.log(curmon)
+        console.log(balanceageday)
+    }
+    else if (month.value == 2) {
+        if ((0 == yr % 4) && (0 != yr % 100) || (0 == yr % 400)) {
+            var balanceageday = Math.abs((29) - (days.value))
+            // console.log(curmon)
+            console.log(balanceageday)
+        }
+        else {
+            var balanceageday = Math.abs((28) - (days.value))
+            // console.log(curmon)
+            console.log(balanceageday)
+        }
+    }
+    else {
+        var balanceageday = Math.abs((30) - (days.value))
+        // console.log(curmon)
+        console.log(balanceageday)
+    }
 month.addEventListener("click", change)
 function change() {
     document.getElementById('days').value=''
